f8069677477484c87abe0a7ca5618fbb5257b074
Rishi Ghan <rishi.ghan@gmail.com>
2021-08-08 13:12:12 -0700

    📙 Volume information fetched and added to the comic model

--- services/import.service.ts
+++ services/import.service.ts
@@ -1,4 +1,5 @@
 "use strict";
+import { isNil } from "lodash";
 import {
 	Context,
 	Service,
@@ -10,7 +11,9 @@ import { DbMixin } from "../mixins/db.mixin";
 import Comic from "../models/comic.model";
 import { walkFolder } from "../utils/file.utils";
 import { convertXMLToJSON } from "../utils/xml.utils";
+import https from "https";
 const ObjectId = require("mongoose").Types.ObjectId;
+
 
 export default class ProductsService extends Service {
 	public constructor(
@@ -83,13 +86,31 @@ export default class ProductsService extends Service {
 								// 2. import payload into sourcedMetadata.comicvine
 								const comicObjectId = new ObjectId(ctx.params.comicObjectId);
 								return new Promise((resolve, reject) => {
-									Comic.findByIdAndUpdate(comicObjectId, { sourcedMetadata: { comicvine: ctx.params.match } }, { new: true}, (err, result) => {
+									Comic.findByIdAndUpdate(comicObjectId, { sourcedMetadata: { comicvine: ctx.params.match } }, { new: true }, (err, result) => {
 										if (err) {
 											console.log(err);
 											reject(err);
 										} else {
-											console.log("RES", result);
-											resolve(result);
+											// 3. Fetch and append volume information
+											if (!isNil(result.sourcedMetadata.comicvine)) {
+												https.get('https://comicvine.gamespot.com/api/volume/4050-50171/?api_key=a5fa0663683df8145a85d694b5da4b87e1c92c69&format=json&limit=1&offset=0&field_list=id,name,deck,image,first_issue,last_issue,publisher,count_of_issues', (resp) => {
+
+													let data = '';
+													resp.on('data', (chunk) => {
+														data += chunk;
+													});
+													
+													resp.on('end', () => {
+														const volumeInformation = JSON.parse(data);
+														result.sourcedMetadata.comicvine.volumeInformation = volumeInformation.results;
+														resolve(result);
+													});
+
+												}).on("error", (err) => {
+													console.log("Error: " + err.message);
+												});
+											}
+											
 										}
 									})
 								});
