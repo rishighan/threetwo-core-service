755ac43820137a945767e02269c52552c349f496
Rishi Ghan <rishi.ghan@gmail.com>
2021-10-21 22:04:58 -0700

    🐇 RabbitMQ connection string changes

--- queue/importQueue.ts
+++ queue/importQueue.ts
@@ -1,28 +1,28 @@
 import { logger } from "../utils/logger.utils";
 //RabbitMQ
 const amqp = require("amqplib/callback_api");
-const rabbitUrl = process.env.DOCKER_RABBITMQ_HOST ? `amqp://${process.env.DOCKER_RABBITMQ_HOST}` : `amqp://localhost`;
+const rabbitUrl = process.env.DOCKER_RABBITMQ_CONNECTION_STRING ? process.env.DOCKER_RABBITMQ_CONNECTION_STRING : `amqp://localhost`;
 
 export const sendToRabbitMQ = (queueName, data) => {
-    // connect to local rabbitmq instance
+	// connect to local rabbitmq instance
 	amqp.connect(rabbitUrl, (error0, connection) => {
 		if (error0) {
 			throw error0;
 		}
-        // create channel
+		// create channel
 		connection.createChannel((error1, channel) => {
 			if (error1) {
 				throw error1;
 			}
 			const queue = queueName;
-            // Checks for “queueName (updateStock)” queue. If it doesn’t exist, then it creates one.
+			// Checks for “queueName (updateStock)” queue. If it doesn’t exist, then it creates one.
 			channel.assertQueue(queue, {
 				durable: false,
 			});
 			channel.sendToQueue(queue, Buffer.from(data));
 			logger.info(`${data} sent`);
 		});
-		setTimeout(function() {
+		setTimeout(function () {
 			connection.close();
 			// process.exit(0);
 		}, 500);
