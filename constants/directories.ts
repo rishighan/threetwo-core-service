b96c57a124a18a7178b9a9ac70b769b6625e9912
Rishi Ghan <rishi.ghan@gmail.com>
2022-03-28 19:29:09 -0700

    🔧 Adjusted the comics and userdata directory paths for docker-compose

--- constants/directories.ts
+++ constants/directories.ts
@@ -1,2 +1,2 @@
-export const COMICS_DIRECTORY = "./comics";
-export const USERDATA_DIRECTORY = "./userdata";
+export const COMICS_DIRECTORY = "/comics";
+export const USERDATA_DIRECTORY = "/userdata";
