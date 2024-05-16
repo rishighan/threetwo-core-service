03f6623ed0e0c98cc774c05c7a915d9dd0020a74
Rishi Ghan <rishi.ghan@gmail.com>
2024-05-15 21:27:38 -0500

    🔧 Fixes

--- config/redis.config.ts
+++ config/redis.config.ts
@@ -1,24 +1,11 @@
 import { createClient } from "redis";
-console.log(`REDIS_URI: ${process.env.REDIS_URI}`);
-const redisURL = process.env.REDIS_URI;
-if (!redisURL) throw new Error("REDIS_URI environment variable is not set.");
 
-const createRedisClient = (url) => {
-	const client = createClient(url);
-
-	client.on("error", (err) => console.error("Redis Client Error", err));
-	client.on("connect", () => console.log("Connected to Redis:", url));
-	client.on("reconnecting", () => console.log("Reconnecting to Redis..."));
-
-	client.connect().catch((err) => console.error("Failed to connect to Redis:", err));
-
-	return client;
-};
-
-const pubClient = createRedisClient(redisURL);
+const pubClient = createClient({
+  url: process.env.REDIS_URI || 'redis://localhost:6379'
+});
+(async () => {
+	await pubClient.connect();
+})();
 const subClient = pubClient.duplicate();
-
-subClient.on("error", (err) => console.error("Redis Subscriber Client Error", err));
-subClient.connect().catch((err) => console.error("Failed to connect Redis Subscriber:", err));
 
 export { subClient, pubClient };
